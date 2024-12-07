ab405405eabc20fe414b627970fa07f3c366ca24
Paolo M <pmorote@gatech.edu>
2024-12-07 18:49:26 -0500

    bp-3 is functional

--- main.js
+++ main.js
@@ -52,29 +52,45 @@ var xAxis = d3.axisBottom(xScale);
 var yAxis = d3.axisLeft(yScale).ticks(6).tickSize(-cellWidth * N, 0, 0);
 // Ordinal color scale for cylinders color mapping
 var customColors = ['#ff5733', '#ffbd69', '#45aaf2', '#2ecc71','#8e44ad','#f7dc6f', '#3498db', '#fd79a8', '#1abc9c', '#f39c12' ];
-var colorScale = d3.scaleOrdinal(customColors);
+const colorScale = d3.scaleLinear()
+    .domain([0,5])
+    .range(["darkred", "green"]);
+//var colorScale = d3.scaleOrdinal(customColors);
 // Map for referencing min/max per each attribute
 var extentByAttribute = [];
 // Object for keeping state of which cell is currently being brushed
 var brushCell;
 
-// ****** Add reusable components here ****** //
+const status_map = new Map([
+    ["0", 'No postseason'],
+    ["1", 'Reached Wild Card Series'],
+    ["2", 'Reached Division Series'],
+    ["3", 'Reached Championship Series'],
+    ["4", 'Reached World Series'],
+    ["5", 'Won World Series']
+  ]);
+
 scatterplot = chartG.append('g').attr('class', 'cell')
     .attr("transform", "translate("+[cellPadding / 2, cellPadding / 2]+")");
         
-sp_xAxis = scatterplot.append('g')
+    sp_xAxis = scatterplot.append('g')
     .attr('class', 'x axis')
-    .attr('transform', 'translate('+[cellPadding / 2, 0]+')')
-sp_xAxis.append('text')
-    .attr('class', 'xaxis-label')
-    .attr('transform', 'translate('+[0, 0]+')');
+    .attr('transform', 'translate('+[cellPadding / 2, 0]+')');
 
 sp_yAxis = scatterplot.append('g')
     .attr('class', 'y axis')
-    .attr('transform', 'translate('+[cellPadding / 2, 0]+')') 
-sp_yAxis.append('text')
+    .attr('transform', 'translate('+[cellPadding / 2, 0]+')');
+scatterplot.append('text')
     .attr('class', 'yaxis-label')
-    .attr('transform', 'translate('+[0, 0]+')rotate(270)');
+    .attr('transform', 'translate('+[-15, 75]+')rotate(270)').text("W/L %");
+
+scatterplot.append('text')
+    .attr('class', 'xaxis-label')
+    .attr('transform', 'translate('+[100, 740]+')').text("2023 Teams");
+
+scatterplot.append('text')
+    .attr('class', 'xaxis-label')
+    .attr('transform', 'translate('+[265, 740]+')').text("Postseason Teams");
 
 var toolTip1 = d3.tip()
     .attr("class", "d3-tip")
@@ -83,35 +99,20 @@ var toolTip1 = d3.tip()
         // Inject html, when creating your html I recommend editing the html within your index.html first
         return "<h5>"+d['Team']+"</h5><table><thead><tr><td>Wins</td><td>Losses</td><td>League</td></tr></thead>"
                         + "<tbody><tr><td>"+d['W']+"</td><td>"+d['L']+"</td><td>"+d['Lg']+"</td></tr></tbody>"
-                        + "</table>"
+                        + "<thead><tr><td>End of Season Status</td></tr></thead>"
+                        + "<tbody><tr><td>"+status_map.get(d['Playoff'])+"</td><</tr></tbody></table>"
     }); 
-
-var toolTip2 = d3.tip()
-    .attr("class", "d3-tip")
-    .offset([-12, 0])
-    .html(function(event, d) {
-        // Inject html, when creating your html I recommend editing the html within your index.html first
-        console.log("Tooltip 2!");
-        return "<h5>"+d['Team']+"</h5><table><thead><tr><td>Wins</td><td>Losses</td><td>League</td></tr></thead>"
-                        + "<tbody><tr><td>"+d['W']+"</td><td>"+d['L']+"</td><td>"+d['Lg']+"</td></tr></tbody>"
-                        + "</table>"
-    }); 
-svg.call(toolTip1);
-svg.call(toolTip2);
 
 d3.csv('data/team_data.csv').then(function(dataset) {
     teams = dataset;
-    playoff_teams = ['ATL', 'BAL', 'HOU', 'MIN', 'TBR', 'TEX', 'TOR', 'LAD', 'MIL', 'PHI', 'MIA', 'ARI'];
     
-    filtered_teams = teams.filter(d => playoff_teams.includes(d['Team']));
     yScale.domain([0.3, 1]);
     xScale.domain([0, 3]);
-    console.log(filtered_teams);
-    sp_xAxis.call(d3.axisBottom(xScale).ticks(0).tickSize(cellHeight, 0, 0))
-    d3.selectAll(".xaxis-label").text("2023 Teams");
+    sp_xAxis.call(d3.axisBottom(xScale).ticks(0).tickSize(cellHeight - cellPadding, 0, 0));
+    //.selectAll(".xaxis-label").text("2023 Teams");
         
-    sp_yAxis.call(d3.axisLeft(yScale).ticks(6).tickSize(-(cellWidth - cellPadding), 0, 0))
-    d3.selectAll(".yaxis-label").text("W/L %");
+    sp_yAxis.call(d3.axisLeft(yScale).ticks(6).tickSize(-(cellWidth - cellPadding), 0, 0));
+    //d3.selectAll(".yaxis-label").text("W/L %");
 
     const spbrush = d3.brushY()
         .extent([[0, 0], [cellWidth - cellPadding, cellHeight - cellPadding]])
@@ -124,7 +125,9 @@ d3.csv('data/team_data.csv').then(function(dataset) {
             .attr('transform', 'translate('+[cellPadding / 2, cellPadding / 2]+')')
             .call(spbrush);
     
-    var circles1 = scatterplot.selectAll("circle")
+    svg.call(toolTip1);
+
+    scatterplot.selectAll(".dotp1")
         .data(dataset).enter()
         .append("circle")
         .attr('class', 'dotp1')
@@ -134,24 +137,21 @@ d3.csv('data/team_data.csv').then(function(dataset) {
         .attr("fill", d => {
             const value = d['Playoff'];
             return value != null ? colorScale(value) : "#000000";
-        });
+        }).on('mouseover', toolTip1.show)
+        .on('mouseout', toolTip1.hide);
 
-    var circles2 = scatterplot.selectAll(".dotp2").data(filtered_teams);
-    circles2.enter()
+    scatterplot.selectAll(".dotp2")
+        .data(dataset.filter(d => +d['Playoff'] > 0)).enter()
         .append("circle")
         .attr('class', 'dotp2')
-        .attr("cx", d => xScale(1.90 + Math.random() * 0.2))
+        .attr("cx", d => xScale(1.9 + Math.random() * 0.2))
         .attr("cy", d => yScale(+d['WL%']))
         .attr("r", 5)
         .attr("fill", d => {
             const value = d['Playoff'];
             return value != null ? colorScale(value) : "#000000";
-        });
-    
-    circles1.on('mouseover', toolTip1.show)
-        .on('mouseout', toolTip1.hide);
-    circles2.on('mouseover', toolTip2.show)
-        .on('mouseout', toolTip2.hide);
+        }).on('mouseover', toolTip1.show)
+        .on('mouseout', toolTip1.hide);;
 
     function spbrushstart(event) {
         // Check if this g element is different than the previous brush
@@ -182,11 +182,6 @@ d3.csv('data/team_data.csv').then(function(dataset) {
                 return e[0] <= yScale(d['WL%']) && yScale(d['WL%']) <= e[1];
             });
 
-            //selectedPoints2 = filtered_teams.filter(function(d){
-            //    return e[0][0] <= xScale(2) && xScale(2) <= e[1][0]
-            //        && e[0][1] <= yScale(d['WL%']) && yScale(d['WL%']) <= e[1][1];
-            //});
-
             svg.selectAll(".dotp1").classed("hidden", true).classed("highlight", false);
             
             svg.selectAll(".dotp1").data(dataset).classed("hidden", d => !selectedPoints.includes(d))
@@ -212,88 +207,3 @@ d3.csv('data/team_data.csv').then(function(dataset) {
 
     
 });
-    
-
-/*
-    var cellEnter = chartG.selectAll('.cell')
-    .data(cells)
-    .enter()
-    .append('g')
-    .attr('class', 'cell')
-    .attr("transform", function(d) {
-        // Start from the far right for columns to get a better looking chart
-        var tx = (N - d.col - 1) * cellWidth + cellPadding / 2;
-        var ty = d.row * cellHeight + cellPadding / 2;
-        return "translate("+[tx, ty]+")";
-     });
-     
-    cellEnter.append('g')
-        .attr('class', 'brush')
-        .call(brush);
-
-    cellEnter.each(function(cell){
-        cell.init(this);
-        cell.update(this, dataset);
-    });
-
-    svg.call(toolTip);
-});
-
-function brushstart(event, cell) {
-    // cell is the SplomCell object
-
-    // Check if this g element is different than the previous brush
-    if(brushCell !== this) {
-
-        // Clear the old brush
-        brush.move(d3.select(brushCell), null);
-
-        // Update the global scales for the subsequent brushmove events
-        xScale.domain(extentByAttribute[cell.x]);
-        yScale.domain(extentByAttribute[cell.y]);
-
-        // Save the state of this g element as having an active brush
-        brushCell = this;
-    }
-}
-
-function brushmove(event, cell) {
-    // cell is the SplomCell object
-
-    // Get the extent or bounding box of the brush event, this is a 2x2 array
-    var e = event.selection;
-    if(e) {
-
-        // Select all .dot circles, and add the "hidden" class if the data for that circle
-        // lies outside of the brush-filter applied for this SplomCells x and y attributes
-        svg.selectAll(".dot")
-            .classed("hidden", function(d){
-                return e[0][0] > xScale(d[cell.x]) || xScale(d[cell.x]) > e[1][0]
-                    || e[0][1] > yScale(d[cell.y]) || yScale(d[cell.y]) > e[1][1];
-            })
-    }
-}
-
-function brushend(event) {
-    // If there is no longer an extent or bounding box then the brush has been removed
-    if(!event.selection) {
-        // Bring back all hidden .dot elements
-        svg.selectAll('.hidden').classed('hidden', false);
-        // Return the state of the active brushCell to be undefined
-        brushCell = undefined;
-    }
-}
-
-function dataPreprocessor(row) {
-    return {
-        'name': row['CerealName'],
-        'calories': +row['Calories'],
-        'protein (g)': +row['Protein_g'],
-        'fat': +row['Fat'],
-        'sodium': +row['Sodium'],
-        'dietary fiber': +row['DietaryFiber'],
-        'carbs': +row['Carbs'],
-        'sugars': +row['Sugars'],
-        'display shelf': +row['DisplayShelf']
-    };
-}*/
