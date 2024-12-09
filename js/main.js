a71faff51dd9dfe6f17ccf463ae8e849afad12b1
Paolo M <pmorote@gatech.edu>
2024-12-08 19:22:03 -0500

    added background

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@
 const dots = document.querySelectorAll('.navdot');
 const pages = document.querySelectorAll('.page');
 const container = document.querySelector('.container');
+const image = document.getElementById('main_field');
+
+const positions = [
+    { top: '50%', rot: '0deg', width: '1000px', left: '70%' }, // Page 1
+    { top: '110%', rot: '45deg', width: '5000px', left: '-100%' }, // Page 2
+    { top: '100%', rot: '0deg', width: '5000px', left: '20%' }, // Page 3
+    { top: '100%', rot: '0deg', width: '5000px', left: '20%' }, // Page 3
+    { top: '100%', rot: '0deg', width: '5000px', left: '20%' }, // Page 3
+    { top: '-270%', rot: '-45deg', width: '5000px', left: '100%' },  // Page 
+    { top: '-270%', rot: '-45deg', width: '5000px', left: '100%' },  // Page 
+    { top: '-270%', rot: '-45deg', width: '5000px', left: '80%' }, // Page 
+    { top: '-270%', rot: '-45deg', width: '5000px', left: '80%' }
+];
 
 dots.forEach(dot => {
     dot.addEventListener('click', () => {
@@ -17,6 +30,12 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             dots.forEach(dot => dot.classList.remove('active'));
             dots[index].classList.add('active');
+
+            const position = positions[index];
+            image.style.top = position.top;
+            image.style.left = position.left;
+            image.style.width = position.width;
+            image.style.rotate = position.rot;
         }
     });
 }, { threshold: 0.7 });
