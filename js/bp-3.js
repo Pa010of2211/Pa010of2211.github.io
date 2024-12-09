41bcefd53d6ca073747c25bfd5bc48b77e26a2b1
Paolo M <pmorote@gatech.edu>
2024-12-08 21:52:39 -0500

    pg 1 and 2 done

--- js/bp-3.js
+++ js/bp-3.js
@@ -1,7 +1,7 @@
-var bp3_svg = d3.select('#p2');
+var bp3_svg = d3.select('#p2_svg');
 
 // Get layout parameters
-var svgWidth = +bp3_svg.attr('width');
+var svgWidth = 600;
 var svgHeight = +bp3_svg.attr('height');
 
 var padding = {t: 40, r: 40, b: 40, l: 40};
@@ -9,7 +9,8 @@ var cellPadding = 10;
 
 // Create a group element for appending chart elements
 var chartG = bp3_svg.append('g')
-    .attr('transform', 'translate('+[padding.l, padding.t]+')');
+.attr('id', 'p2_g')
+.attr('transform', 'translate('+[padding.l, padding.t]+')');
 
 var N = 1;
 
@@ -63,6 +64,7 @@ scatterplot.append('text')
 
 var toolTip1 = d3.tip()
     .attr("class", "d3-tip")
+    .style("z-index", "3")
     .offset([-12, 0])
     .html(function(event, d) {
         // Inject html, when creating your html I recommend editing the html within your index.html first
@@ -71,6 +73,69 @@ var toolTip1 = d3.tip()
                         + "<thead><tr><td>End of Season Status</td></tr></thead>"
                         + "<tbody><tr><td>"+status_map.get(d['Playoff'])+"</td><</tr></tbody></table>"
     }); 
+
+const boxX = 650;
+const boxY = 300; 
+const boxWidth = 300;
+const boxHeight = 150;
+const boxColor = "#cce7ff";
+const textLines = ["The Texas Rangers and", "Arizona Diamondbacks are", "#6 and #12 in term of WL%"];
+// Add a box
+bp3_svg.append("rect")
+    .attr("x", boxX)
+    .attr("y", boxY)
+    .attr("width", boxWidth)
+    .attr("height", boxHeight)
+    .attr("fill", boxColor)
+    .attr("stroke", "black")
+    .attr("stroke-width", 2);
+
+const textGroup = bp3_svg.append("text")
+    .attr("x", boxX + boxWidth / 2) 
+    .attr("y", boxY + 55) 
+    .attr("text-anchor", "middle") 
+    .style("font-family", "Arial")
+    .style("font-size", "24px")
+    .style("fill", "black");
+
+// Add multiple lines of text using tspans
+textLines.forEach((line, index) => {
+    textGroup.append("tspan")
+        .attr("x", boxX + boxWidth / 2) // Center horizontally
+        .attr("dy", index === 0 ? "0" : "1.2em") // Offset each line (first line stays at y)
+        .text(line);
+});
+
+const textLines2 = ["This indicates that winning a", "lot of games is not all", "it takes to make it to the post", "season or even to win","the world series"];
+
+const textGroup2 = bp3_svg.append("text")
+    .attr("x", boxX + (boxWidth+50) / 2) 
+    .attr("y", boxY + 250) 
+    .attr("text-anchor", "middle") 
+    .style("font-family", "Arial")
+    .style("font-size", "24px")
+    .style("fill", "white");
+
+// Add multiple lines of text using tspans
+textLines2.forEach((line, index) => {
+    textGroup2.append("tspan")
+        .attr("x", boxX + boxWidth / 2) // Center horizontally
+        .attr("dy", index === 0 ? "0" : "1.2em") // Offset each line (first line stays at y)
+        .text(line);
+});
+//Arrowhead
+bp3_svg.append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 5)
+    .attr("refY", 0)
+    .attr("markerWidth", 6) 
+    .attr("markerHeight", 6)
+    .attr("orient", "auto") 
+    .append("path")
+    .attr("d", "M0,-5L10,0L0,5") 
+    .style("fill", "steelblue"); 
 
 d3.csv('data/team_data.csv').then(function(dataset) {
     teams = dataset;
@@ -88,13 +153,13 @@ d3.csv('data/team_data.csv').then(function(dataset) {
         .on("start", spbrushstart)
         .on("brush", spbrushmove)
         .on("end", spbrushend);
-
+    bp3_svg.call(toolTip1);
     scatterplot.append("g")
             .attr("class", "brush")
             .attr('transform', 'translate('+[cellPadding / 2, cellPadding / 2]+')')
             .call(spbrush);
     
-    bp3_svg.call(toolTip1);
+    
 
     sp_points = scatterplot.selectAll(".bp3_dot")
         .data(dataset).enter()
@@ -114,6 +179,21 @@ d3.csv('data/team_data.csv').then(function(dataset) {
             return value != null ? colorScale(value) : "#000000";
         }).on('mouseover', toolTip1.show)
         .on('mouseout', toolTip1.hide);
+    
+    dataset.filter(d => +d['Playoff'] > 3)
+        .forEach(d => {
+            const startX = bp3_xScale(2.1) + 300; 
+            const startY = bp3_yScale(+d['WL%']) - 110;
+
+            scatterplot.append("line")
+            .attr("class", "arrow") 
+                .attr("x1", startX)
+                .attr("y1", startY)
+                .attr("x2", bp3_xScale(2.1))
+                .attr("y2", bp3_yScale(+d['WL%']))
+                .attr("marker-end", "url(#arrowhead)")
+                .attr('stroke', 'steelblue').attr('fill', 'none').attr('stroke-width', '4px');
+            });
 
     function spbrushstart(event) {
         // Check if this g element is different than the previous brush
@@ -148,11 +228,6 @@ d3.csv('data/team_data.csv').then(function(dataset) {
             
             bp3_svg.selectAll(".bp3_dot").data(dataset).classed("hidden", d => !selectedPoints.includes(d))
                 .classed("highlight", d => selectedPoints.includes(d));
-            /*
-            svg.selectAll(".dotp2").classed("hidden", true).classed("highlight", false);
-        
-            svg.selectAll(".dotp2").data(dataset).classed("hidden", d => !selectedPoints.includes(d))
-                .classed("highlight", d => selectedPoints.includes(d));*/
         }
     }
 
